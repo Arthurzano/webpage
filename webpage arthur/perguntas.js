7df0cbd74e9ce7e722d00583b5d84ac7a4973121
Arthurzano <159539643+Arthurzano@users.noreply.github.com>
2024-09-30 07:51:16 -0300

    Add files via upload

---
+++ webpage arthur/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Agentes',
+    'Qual agente tem poder de fogo?',
+    'Phoenix'
+)
+
+criaCartao(
+    'Mapas',
+    'Quais mapas tem 3 bombs?',
+    'Haven e Lotus'
+)
+
+criaCartao(
+    'Armas',
+    'Qual arma mata com 1 unico tiro no peito',
+    'operator'
+)
+
+criaCartao(
+    'Falas',
+    'Quem fala a frase "eu sou o caçador"?',
+    'Sova na ultimate'
+)
